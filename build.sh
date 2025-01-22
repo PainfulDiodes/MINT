425b5d3aa746cadf20bff71d76c8c55ebb264b8a
Stephen Willcock <stephen.willcock@gmail.com>
2025-01-22 15:17:37 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-# sjasmplus --raw=bin --lst=BeanZee.z80.lst BeanZee.z80
-hexdump -C BeanZee.z80.bin > BeanZee.z80.C.hex
+# use: ./build.sh BeanZee
+zmac $1.z80
+cp zout/$1.cim $1.z80.bin
+hexdump -C $1.z80.bin > $1.z80.C.hex
